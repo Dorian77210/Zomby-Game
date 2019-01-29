92c06e14d63739db4b5158e2017004da4e2625ce
Terbah <dorian.terb@gmail.com>
2019-01-29 13:25:49 +0100

    First commit

--- src/controller/base/ActionController.java
+++ src/controller/base/ActionController.java
@@ -1,9 +1,22 @@
 package controller.base;
 
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
   * The class <code>ActionController</code> is the base controller for the buttons, fields etc...
   * @version 1.0
   * @author Dorian Terbah 
 **/
+
+public class ActionController implements ActionListener {
+
+    public ActionController() {
+
+    }
+
+    @Override 
+    public void actionPerformed(ActionEvent event) {
+        
+    }
+}
